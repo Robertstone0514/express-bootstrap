76fdbfcf0535f82633c426a93ae4aa908004a509
Antonia Symeonidou <antonia.symeonidou@outlook.com>
2020-06-16 19:48:44 +0100

    moving controller to correct file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 const express = require('express');
+const { mainController } = require('./controller');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({
-    message: 'Hello world!',
-  });
-});
+app.get('/', mainController);
 
 module.exports = app;
